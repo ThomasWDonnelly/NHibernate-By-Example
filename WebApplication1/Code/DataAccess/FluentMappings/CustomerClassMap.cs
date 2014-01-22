81e41009dcc0b479b816765857ee65ba556c0335
David Whitney <david@davidwhitney.co.uk>
2014-01-22 23:22:56 +0000

    Mapped the customer, but the table names are all wrong so data won't load...

---
+++ WebApplication1/Code/DataAccess/FluentMappings/CustomerClassMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using WebApplication1.Code.Model;
+
+namespace WebApplication1.Code.DataAccess.FluentMappings
+{
+    /// <summary>
+    /// This is the simplest possible mapping.
+    /// It'll work because FluentNHibernate uses reasonably sane defaults.
+    /// </summary>
+    public class CustomerClassMap : ClassMap<Customer>
+    {
+        public CustomerClassMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+            Map(x => x.Gender);
+        }
+    }
+}
