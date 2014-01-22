bb9af1750d49e7b8a1ac39b158b1b7dac3dc93b2
David Whitney <david@davidwhitney.co.uk>
2014-01-22 23:47:58 +0000

    Flush() example.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -16,38 +16,28 @@ namespace WebApplication1.Controllers
 
         public ActionResult Index()
         {
-            // We've switched to a Get rather than a load here
-            // To steal this excellent StackOverflow explaination
+            // So we want to modify some data?
+            // This is how you might think you should do it...
 
-            /* Load should be used when you know for sure that an entity with a certain ID exists.
-             * The call does not result in a database hit (and thus can be optimized away by NHibernate in certain cases).
-             * Beware of the exception that may be raised when the object is accessed if the entity instance
-             * doesn't exist in the DB.
-             * 
-             * Get instantly hits the database to retrieve the entity data. 
-             * If the entity exists it is returned, otherwise null will be returned.
-             * This is the safest way to determine whether an entity with a certain ID exists or not. 
-             * If you're not sure what to use, use Get.             
+            
+            var customer = _session.Get<Customer>(1);
+            customer.Name = "NameGoesHere_" + Guid.NewGuid();
+            _session.Save(customer);
+            _session.Flush();
+
+            // Hey look! That worked.
+            // Except it's not what you're meant to do.
+
+            // Flush:
+
+            /* If you happen to be using the ITransaction API, you don't need to worry about this step. 
+             * It will be performed implicitly when the transaction is committed. 
+             * Otherwise you should call ISession.Flush() to ensure that all changes are synchronized with the database.
              */
 
-            // Before we changes the class map to support our enum, the call to .Load
-            // appeared as though it was working - beware this in conjunction with mapping errors
-            // as you'll end up with strange bugs where your entities appear to load, but they haven't.
-
-            var customer = _session.Get<Customer>(1);
-
-
-            // Now, not really knowing how to use nhibernate, we want to change some data.
-            customer.Name = customer.Name + "_" + Guid.NewGuid();
-            _session.Save(customer);
-
-            // Awesome! looks like it'll work right?
-            // But guess what, debug through this page load twice - notice how nothing changes?
-            // That's because gotcha! _session.Save() doesn't actually save the data!
-            // Save and SaveOrUpdate associate an item with nhibernates change tracking - they're a statement of intent
-            // They say "I want to save this thing when you flush the session".
-
-            // Guess what we've not done?
+            // Flush basically just round-trips to the database, and actually, nhibernate is way smarter than that.
+            // Actually, if you use a transaction, nhibernate better optimises it's round-tripping, and you
+            // should never call flush manually.
 
             return View();
         }
