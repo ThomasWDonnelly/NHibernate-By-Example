99098e877675f49fa428b6cf67488570f4fd7183
David Whitney <david@davidwhitney.co.uk>
2014-01-23 00:40:17 +0000

    Various types of query examples.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -2,42 +2,68 @@
 using System.Diagnostics;
 using System.Web.Mvc;
 using NHibernate;
+using NHibernate.Criterion;
 using WebApplication1.Code.Model;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly CustomerRepository _customerRepo;
+        private readonly ISession _session;
         
-        public HomeController(ISession session, CustomerRepository customerRepo)
+        public HomeController(ISession session)
         {
-            _customerRepo = customerRepo;
+            _session = session;
         }
 
         public ActionResult Index()
         {
-            // At this point it's common for people to implement repositories over NHibernate.
-            // It's not a good idea, but here's an example of the BAD CODE
+            // Back to our original sample, the more astute of you will start to think about queries.
+            // Given the general advice of "don't use repositories" with NHibernate, you'll realise that
+            // organising queries logically could be a problem.
 
-            var customer = _customerRepo.Get(1);
+            // Before we get into that, lets look at the most popular ways to query with NHibernate...
 
-            // This is sucky for several reasons.
-            // 1. Useless indirection - You've just implemented the exact same features as were available on ISession
-            // 2. Constructor explosion - All of a sudden, you need a repo per aggregate root, and often inject many of them at once
-            // 3. You start to loose API features by abstracting away from the ORM - you end up implementing "save or update or saveorupdate" logic
-            // 4. You realise that repositories are meant to be transaction boundaries and start implementing Flush()'s
-            // 5. ... at this point, hindering NHibernates ability to do its job well.
-            // 6. You end up writing loads of boilerplate repositories.
-            // 7. You get angry and say NHibernate doesn't perform
-            // 8. Actually it's just poor design.
+            // First lets take a look at query over...
 
-            // People do this with the belief that "we can just migrate to another ORM trivially".
-            // It's not trivial to migrate ORMS, you're not really protecting yourself, all you're doing
-            // is restricting yourself to the subset of features of the ORM you're using that are common
-            // with a hand rolled DAL.
+            var results =
+                _session.QueryOver<Customer>()
+                    .WhereRestrictionOn(x => x.Name)
+                    .IsLike("Rich", MatchMode.Anywhere)
+                    .List<Customer>();
 
-            // The ISession is your repository. Use it as one.
+            // You'll notice that QueryOver is not a Linq provider. This is often a source of confusion and frustration
+            // as people expect things like this to work:
+
+            try
+            {
+                var throwsAnException = _session.QueryOver<Customer>().Where(x => x.Name.StartsWith("Rich")).List<Customer>();
+            } catch { }
+
+            // While this compiles, and uses IQueryable expressions, most of the operations are not implemented.
+            // It's especially confusing, because very simple operations do work, such as this:
+
+            var worksBecauseItsSimple =
+                _session.QueryOver<Customer>().Where(x => x.Name == "Richard Whitney").List<Customer>();
+
+            // Unfortunately, if you don't know what is and isn't implemented, you'll get caught out and it's not obvious
+            // which operations are happening in app memory, and which are running on the Db.
+            // Avoid using straight Linq for all but the simplest of queries.
+
+            // Then there's the classic Criteria API - this was the "original" query syntax of N/Hibernate, and it's
+            // what query-over generates under the hood with the help of a few expression trees.
+
+            var classic = _session.CreateCriteria<Customer>()
+                .Add(Restrictions.Eq("Name", "Richard Whitney"))
+                .List<Customer>();
+
+            // It's reliable, but the syntax is full of magic strings, and you should do your best to 
+            // ALWAYS prefer the QueryOver<> API.
+            
+
+            // Then there's HQL. Don't use it. It's powerful. Don't use it. 
+            // We'll come back later when dealing with "advanced queries"
+
 
 
             return View();
